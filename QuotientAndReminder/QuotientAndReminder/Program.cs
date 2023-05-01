03b1339fb5cebcbf3124f9b1301854619e486005
Rahulalvk <rahulal432@gmail.com>
2023-05-01 02:16:29 -0700

    Quotient and Reminder program

---
+++ QuotientAndReminder/QuotientAndReminder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuotientAndReminder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Dividend");
+            int Dividend =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Divisor");
+            int Divisor = Convert.ToInt32(Console.ReadLine());
+
+            float Quotient = Dividend / Divisor;
+            float Reminder = Dividend % Divisor;
+            Console.WriteLine("Quotient ="+Quotient);
+            Console.WriteLine("Reminder ="+Reminder);
+        }
+    }
+}
